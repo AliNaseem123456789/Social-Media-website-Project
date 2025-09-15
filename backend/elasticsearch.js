63ad930833f3d1f643a5a04e168ec069764cfed9
AliNaseem123456789 <139787525+AliNaseem123456789@users.noreply.github.com>
2025-09-15 14:08:32 +0500

    Update elasticsearch.js

--- backend/elasticsearch.js
+++ backend/elasticsearch.js
@@ -5,7 +5,7 @@ import { Client } from "@elastic/elasticsearch";
 const client = new Client({
   node: "https://my-elasticsearch-project-aa83ed.es.us-central1.gcp.elastic.cloud:443",
   auth: {
-    apiKey: "NDV1RlRKa0JWam40dEtUMkdhQkw6Qk1IcXpnYXVLeVlKUXkyVHN3VVAwUQ==", // paste the one you showed me
+    apiKey: "NDV1RlRKa0JWam40dEtUMkdhQkw6Qk1IcXpnYXVLeVlKUXkyVHN3VVAwUQ==", 
 
   },
 });
